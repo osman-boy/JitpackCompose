9780bb12b6805a23bb7b510f91e289f6708b0e69
osman-boy <osulejmanov3@gmail.com>
2022-02-28 22:17:43 +0600

    Initial commit

---
+++ app/src/main/java/com/android/jitpackcomposeexample/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.android.jitpackcomposeexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(Modifier.verticalScroll(rememberScrollState())) {
+
+                ListItem(name = "John", lastname = "Wick", profession = "Killer")
+                ListItem(name = "John", lastname = "Wick", profession = "Killer")
+                ListItem(name = "John", lastname = "Wick", profession = "Killer")
+                ListItem(name = "John", lastname = "Wick", profession = "Killer")
+                ListItem(name = "John", lastname = "Wick", profession = "Killer")
+                ListItem(name = "John", lastname = "Wick", profession = "Killer")
+                ListItem(name = "John", lastname = "Wick", profession = "Killer")
+                ListItem(name = "John", lastname = "Wick", profession = "Killer")
+                ListItem(name = "John", lastname = "Wick", profession = "Killer")
+                ListItem(name = "John", lastname = "Wick", profession = "Killer")
+            }
+        }
+    }
+}
+
+
+@Composable
+fun ListItem(name: String, lastname: String, profession: String) {
+    val counter = remember {
+        mutableStateOf(0)
+    }
+    val color:MutableState<Color> = remember {
+        mutableStateOf(Color.Black)
+    }
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable {
+                color.value=  when (++counter.value) {
+                    in 0..5 -> Color.Black
+                    in 6..11 -> Color.Red
+                    in 12..18 -> Color.Green
+                    in 19..25 -> Color.Yellow
+                    else->Color.Black
+                }
+
+            },
+        shape = RoundedCornerShape(16.dp),
+        elevation = 4.dp,
+    ) {
+        Box {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.img),
+                    contentDescription = "User is image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
+                Column(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text(text = "$name $lastname", fontSize = 16.sp)
+                    Text(text = profession)
+                    Text(
+                        text = "killed person count: ${counter.value}",
+                        color = color.value
+                    )
+                }
+
+
+            }
+        }
+    }
+}
